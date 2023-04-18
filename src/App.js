43fde74e91693cb4819c0997d3786ef8518fd132
Alexey <alexey@Alexeys-iMac.local>
2023-04-18 10:04:53 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react"
+import {validateEmail} from "../src/utils"
+import PasswordErrorMessage from "../src/components/PasswordErrorMessage"
 
 function App() {
+
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState({
+    value: "",
+    isTouched: false
+  })
+  const [role, setRole] = useState("role")
+
+  const getIsFormValid = () => {
+    return (
+      firstName && 
+      validateEmail(email) && 
+      password.value.length >= 8 &&
+      role !== "role"
+    )
+  }
+
+  const clearForm = () => {
+    setFirstName("")
+    setLastName("")
+    setEmail("")
+    setPassword({
+      value:"",
+      isTouched: false,
+    })
+    setRole("role")
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert("account created mfaka")
+    clearForm()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <h2>Sign Up, Loser</h2>
+          <div className="Field">
+            <label>
+              First Name <sup>*</sup>  
+            </label>
+            <input value={firstName}
+              onChange={(e) =>{
+                setFirstName(e.target.value)
+              }}
+              placeholder="First Name"
+            />
+          </div>
+          <div className="Field">
+            <label>
+              Last Name <sup>*</sup>
+              <input
+                value={lastName}
+                onChange={(e) => {
+                  setLastName(e.target.value)
+                }}
+                placeholder="Last Name"
+              />
+            </label>
+          </div>
+          <div classname="Field">
+            <label>
+              Email <sup>*</sup>
+            </label>
+            <input value={email}
+              onChange= {(e) => {
+                setEmail(e.target.value)
+              }}
+              placeholder="Email Address"
+            />
+          </div>
+          <div className="Field">
+              <label>
+                Password <sup>*</sup>
+              </label>
+              <input
+                value={setPassword.value}
+                placeholder='Password'
+                type='password'
+                onChange={(e) => {
+                  setPassword({...password, value: e.target.value})
+                }}
+                onBlur={() => {
+                  setPassword({...password, isTouched: true})
+                }}
+              />
+              {password.isTouched && password.value.length < 8 ? (
+                <PasswordErrorMessage />
+              ) : null}
+          </div>
+          <div className="Field">
+            <label>
+              Role <sup>*</sup>
+            </label>
+            <select value={role} onChange={(e) => setRole(e.target.value)}>
+              <option value="role">Role</option>
+              <option value="individual">Individual</option>
+              <option value="business">Business</option>
+            </select>
+          </div>
+          <button type="submit" disabled={!getIsFormValid()}>Create Account</button>
+        </fieldset>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/PasswordErrorMessage.js
@@ -0,0 +1,8 @@
+
+const PasswordErrorMessage = () => {
+    return(
+      <p className="Field Error">Password should have at least 8 characters, you stupid bitch</p>
+    )
+  }
+
+export default PasswordErrorMessage
